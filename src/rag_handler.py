838cd6a072919260cd636325d3f7e7a6a04821ea
Priam.Cardouat <priam.cardouat@gfk.com>
2024-06-09 16:56:18 +0100

    Improved the rag handler

--- src/rag_handler.py
+++ src/rag_handler.py
@@ -1,17 +1,16 @@
 """Classes to handle RAG related actions."""
 import os
+import shutil
 
 import torch
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.vectorstores import FAISS
-from langchain_community.document_loaders import BSHTMLLoader, DirectoryLoader
+from langchain_community.document_loaders import DirectoryLoader, TextLoader
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
 from langchain_core.documents import Document
-from langchain_text_splitters import HTMLSectionSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 DATA_DIR = os.path.join(os.path.dirname(__file__), "..", "data")
-HEADERS_TO_SPLIT_ON = [(f"h{i + 1}", f"Header{i + 1}") for i in range(15)]
-
-
+os.environ["CURL_CA_BUNDLE"] = ""
 CONFIG = {}
 
 
@@ -28,9 +27,12 @@ class RAGHandler:
 
     def __init__(
         self,
-        model: str = "sentence-transformers/all-mpnet-base-v2",
-        nb_docs_returned: int = 20,
+        model: str = "lewispons/email-classifiers",
+        nb_docs_returned: int = 5,
         fetch_k_docs: int = 200,
+        chunk_size: int = 1000,
+        chunk_overlap: int = 20,
+        threshold: float = 0.8,
     ):
         """Initialise the class."""
         self.documents = None
@@ -40,6 +42,9 @@ class RAGHandler:
         self.embedding = None
         self.nb_docs_returned = nb_docs_returned
         self.fetch_k_docs = fetch_k_docs
+        self.chunk_size = chunk_size
+        self.chunk_overlap = chunk_overlap
+        self.threshold = threshold
 
     def set_nb_docs_returned(self, nb_docs_returned):
         """Set nb_docs_returned."""
@@ -49,21 +54,50 @@ class RAGHandler:
         """Set fetch_k_docs."""
         self.fetch_k_docs = fetch_k_docs
 
+    def set_chunk_size(self, chunk_size):
+        """Set the chunk_size."""
+        self.chunk_size = chunk_size
+
+    def set_chunk_overlap(self, chunk_overlap):
+        """Set the chunk_size."""
+        self.chunk_overlap = chunk_overlap
+
     def load_documents(self):
         """Load the documents."""
-        loader = DirectoryLoader("data", show_progress=True, loader_cls=BSHTMLLoader, silent_errors=True)
+        print("Load emails for ingestion to vector store.")
+        loader = DirectoryLoader("data", show_progress=True, loader_cls=TextLoader, silent_errors=True)
+        print("Email loading successful.")
         self.documents = loader.load()
 
     def split_documents(self):
         """Split the documents."""
-        html_splitter = HTMLSectionSplitter(headers_to_split_on=HEADERS_TO_SPLIT_ON)
-        self.split_docs = html_splitter.split_documents(self.documents)
+        print("Start emails splitting for vector store ingestion.")
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=self.chunk_size, chunk_overlap=self.chunk_overlap)
+        print("Emails splitting successful.")
+        self.split_docs = text_splitter.split_documents(self.documents)
+
+    def set_embeddings(self):
+        """Set the embedding with appropriate model."""
+        embeddings = HuggingFaceEmbeddings(model_name=self.model, model_kwargs=CONFIG)
+        self.embedding = embeddings
 
     def set_vectorestore(self):
         """Set the vectore store."""
+        print("Start setting up the vector store.")
+        set_config()
         embeddings = HuggingFaceEmbeddings(model_name=self.model, model_kwargs=CONFIG)
         self.vectorstore = FAISS.from_documents(self.split_docs, embeddings)
         self.embedding = embeddings
+        print("Vector store set up successfully.")
+        print("Cleaning...")
+        for item in os.listdir("./data/"):
+            if item not in [".gitkeep", ".DS_Store"]:
+                try:
+                    shutil.rmtree("./data/" + item)
+                except Exception as e:
+                    print(f"Something went wrong: {e}")
+                    raise e
+        print("Cleaning done.")
 
     def save_vectorestore(self):
         """Save the vectore store."""
@@ -81,4 +115,25 @@ class RAGHandler:
         result = self.vectorstore.similarity_search_with_score(  # type: ignore[attr-defined]
             query, k=self.nb_docs_returned, fetch_k=self.fetch_k_docs
         )
-        return result
+        print("Documents found: ")
+        filtered_res: list[tuple[Document, float]] = []
+        for doc, score in result:
+            if score < self.threshold:
+                filtered_res.append((doc, score))
+                print(doc.page_content)
+                print(score)
+                print()
+        return filtered_res
+
+    def get_context(self, query) -> str:
+        """Get context for a query."""
+        context_docs = self.query_vectorestore(query)
+        context_text = "\n------\n".join([doc.page_content for doc, _ in context_docs])
+        return context_text
+
+    def initialise_set_up(self):
+        """Initialise the set up."""
+        self.load_documents()
+        self.split_documents()
+        self.set_vectorestore()
+        self.save_vectorestore()
